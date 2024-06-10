5e8a970c2483f7ebeb232a6fcff6f543102b8bd4
Naomi <naomi.d.braun@gmail.com>
2024-06-10 16:37:14 -0400

    Add timestamp conversion endpoint

--- index.js
+++ index.js
@@ -24,6 +24,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date_string?", function(req, res){
+  var response,
+    date,
+    unix_dt,
+    utc_dt;
+
+  // If :date_string is empty, return current time
+  if (!req.params.date_string) {
+    unix_dt = Date.now()
+    utc_dt = new Date(unix_dt).toUTCString();
+    response = ({ unix: unix_dt, utc: utc_dt });
+  } else {
+    // If :date_string is valid date, return UTC date and Unix timestamp in milliseconds
+    if (Number(req.params.date_string)) {
+      // UNIX input
+      date = new Date(Number(req.params.date_string));
+    } else {
+      // UTC input
+      date = new Date(req.params.date_string);
+    }
+
+    if (!isNaN(date)) {
+      unix_dt = date.getTime();
+      utc_dt = date.toUTCString();
+      response = ({ unix: unix_dt, utc: utc_dt });
+    } else {
+      // If :date_string is invalid, return error
+      response = ({ error: "Invalid Date" })
+    }
+  }
+  res.json(response);
+})
 
 
 // Listen on port set in environment variable or default to 3000
